fb279cb2b787e72fcc09279437e070ce108a7ea1
Esubalew197 <esubalewmillion@gmail.com>
2023-05-10 00:29:19 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,66 @@
 #!/usr/bin/python3
-"""Contains recurse function to get hot topics"""
+""" Contains recurse function to get hot topics """
+
+
 import requests
+url_base = "https://www.reddit.com/r/"
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {
-        "User-Agent": "REDDTAPI/0.0.1"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recursor(subreddit, lst, url):
+    """ implementation of recursion to get multi pages """
+    headers = {'user-agent': 'my-app/0.0.1'}
+    r = requests.get(url, headers=headers)
+    #  print("entered")
+    if r.status_code == 200:
+        try:
+            data = r.json()
+            list_of_subreddits = data['data']['children']
+            #  for sub_r in list_of_subreddits:
+            #    lst.append(sub_r['data']['title'])
+            titles = [x.get('data').get('title') for x in list_of_subreddits]
+            lst.extend(titles)
+            after = data['data']['after']
+            if after:
+                url = url_base + subreddit + "/hot.json?raw_json=1&after=" + \
+                    after
+                return recursor(subreddit, lst, url)
+            else:
+                return lst
+        except Exception:
+            pass
+
+
+def recurse(subreddit, hot_list=[]):
+    """ gets the number of subscribers of a subreddit """
+    url = url_base + subreddit + "/hot.json?raw_json=1"
+    headers = {'user-agent': 'my-app/0.0.1'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        try:
+            data = r.json()
+            list_of_subreddits = data['data']['children']
+            #  for sub_r in list_of_subreddits:
+            #    hot_list.append(sub_r['data']['title'])
+            titles = [x.get('data').get('title') for x in list_of_subreddits]
+            hot_list.extend(titles)
+            after = data['data']['after']
+            if after:
+                url = url_base + subreddit + "/hot.json?raw_json=1&after=" + \
+                    after
+                return recursor(subreddit, hot_list, url)
+            else:
+                return hot_list
+        except Exception:
+            pass
+    else:
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("provide url")
+        sys.exit(1)
+    else:
+        res = recurse(sys.argv[1], [])
+        print(res, "\n", len(res))
